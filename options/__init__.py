bd1435ca80fb60406996c2b9f3effebf07208872
Jin Xi <249589575@qq.com>
2023-03-29 00:26:48 +0800

    参数工具

---
+++ options/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2023 Xi Jin
+# Fake it. Util make it. Start it, Util love it
+
+"""基础参数"""
---
+++ util/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2023 Xi Jin
+# Fake it. Util make it. Start it, Util love it
+
+"""一些工具方法"""
