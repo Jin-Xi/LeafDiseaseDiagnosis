6c5cd971a1fb4a6fc548dd801324ffcd993288f9
Jin <249589575@qq.com>
2023-03-31 01:54:10 +0800

    完成default transform

--- data/plantdoc_dataset.py
+++ data/plantdoc_dataset.py
@@ -6,13 +6,28 @@ import random
 
 from PIL import Image
 import torch
+from torchvision.transforms import transforms
 
 from data.base_dataset import BaseDataset
 from data.image_folder import ImageFolder, default_loader
 from utils.utils import class2index
 
 
-# TODO:torch transform
+def get_defaut_transforms(img_size, is_train=True):
+    train_trans = transforms.Compose([
+                transforms.RandomSizedCrop(max(img_size)),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    val_trans = transforms.Compose([
+            #Higher scale-up for inception
+            transforms.Scale(int(max(img_size)/224*256)),
+            transforms.CenterCrop(max(img_size)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    return train_trans, val_trans
 
 
 class PlantDocDataset(ImageFolder):
@@ -29,12 +44,14 @@ class PlantDocDataset(ImageFolder):
                 self.image_tuple.append((os.path.join(class_path, path), torch.tensor(self.class2index[class_name])))
 
         self.loader = default_loader
-        # FIXME: 写一个本数据集专用的transform方法
-        self.transform = transform
+        self.transform = get_defaut_transforms(opt.img_size, opt.is_train)
         self.target_transform = target_transform
 
     # TODO:完成数据迭代方法
     def __getitem__(self, index):
+        img_path, label = self.image_tuple[index]
+        img = self.loader(img_path)
+
         pass
 
     def __len__(self):
