4c50d6d1fa4d02044c4334077e7e1fbf76221b27
Jin Xi <249589575@qq.com>
2023-04-04 01:23:08 +0800

    完成csv生成工具

--- utils/gen_csv.py
+++ utils/gen_csv.py
@@ -3,39 +3,68 @@
 
 
 import os
+import sys
 from collections import Counter
 
+import torch
 import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
 
-from data.image_folder import IMG_EXTENSIONS
-from data.image_folder import is_image_file, make_dataset
+
+# 可用图像类型
+IMG_EXTENSIONS = [
+    '.jpg', '.JPG', '.jpeg', '.JPEG',
+    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+    '.tif', '.TIF', '.tiff', '.TIFF',
+]
 
 
-def gen_csv(dataset_src='PlantVillage'):
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+
+def make_dataset(dir, max_dataset_size=float("inf")):
+    images = []
+    assert os.path.isdir(dir), '%s ——> 没有此目录' % dir
+
+    for root, _, fnames in sorted(os.walk(dir)):
+        for fname in fnames:
+            if is_image_file(fname):
+                path = os.path.join(root, fname)
+                images.append(path)
+    return images[:min(max_dataset_size, len(images))]
+
+
+def gen_csv(dataset_src='PlantVillage', dataset_name='PlantVillage'):
     """通过数据文件夹生成数据csv文件
 
     Args:
         stc_dataset (str, optional): _description_. Defaults to 'PlantVillage'.
     """
-    pwd = os.getcwd() + '\\datasets\\' + dataset_src
+    pwd = os.getcwd() + '/datasets/' + dataset_src
     images = make_dataset(pwd)
     # 取出labels
     labels = []
     for img in images:
-        labels.append(img.split('\\')[-2])
+        labels.append(img.split('/')[-2])
     # 统计labels
     count = Counter(labels)
-    # TODO:标签序列化器初始化
-    label_binarizer = MultiLabelBinarizer()
-    onehot_labels = label_binarizer.fit_transform(list(count.keys()))
-    
-    # 构建一个标签到onehot的映射
-    labels2array = {}
-    for label, arr in zip(label_binarizer.classes_, onehot_labels):
-        labels2array[label] = arr.tolist()
-    n_classes = len(label_binarizer.classes_)
+
+    # 标签序列化
+    labels = list(count.keys())
+    onehot_encoding = torch.nn.functional.one_hot(torch.arange(0, len(count.keys()))).tolist()
+
+    # 构建label2encoding字典
+    label2encoding = {}
+    for i, label in enumerate(labels):
+        label2encoding[label] = onehot_encoding[i]
+    df = pd.DataFrame(columns=['img_src', 'label', 'one_hot'])
+    for index, (img, label) in enumerate(zip(images, labels)):
+        df.loc[index] = [img, label, label2encoding[label]]
+
+    # 保存csv文件
+    df.to_csv('./datasets/'+dataset_name+'.csv')
 
 
 if __name__ == '__main__':
-    gen_csv()
+    gen_csv('PlantDoc/train')
