bb2144a5fb5bf218392e319472d7a0cf1a49939a
Jin <249589575@qq.com>
2023-04-05 02:32:09 +0800

    测试文件

---
+++ test.py
@@ -0,0 +1,11 @@
+from data.plantdoc_dataset import PlantDocDataset
+from options.train_options import TrainOptions
+
+
+def main():
+    opt = TrainOptions().parse()
+
+
+if __name__ == '__main__':
+    main()
+    
